29a3fbcd47c3c7a849c3f52b90ea0011eccd64a8
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-06-04 12:00:11 +0200

    edited useBalanceContext to throw type error

--- src/context/BalanceProvider.tsx
+++ src/context/BalanceProvider.tsx
@@ -1,17 +1,26 @@
 import { createContext, useContext, useState } from "react";
 import { calculateBalance } from "../utils/calculateBalance";
-import { BalanceContextInterface, ProviderProps } from "../types/types";
+import {
+  BalanceContextInterface,
+  ProviderProps,
+  arrayData,
+} from "../types/types";
 import generateEarningsData from "../data/generateEarningsData";
 import generateExpensesData from "../data/generateExpensesData";
 import { calculatePercentageFirstTwoElements } from "../utils/calculatePercentageFirstTwoElements";
 import { formatChartData } from "../utils/formatChartData";
-import { arrayData } from "../types/types";
 
 const BalanceContext = createContext<BalanceContextInterface | undefined>(
   undefined
 );
 
-export const useBalanceContext = () => useContext(BalanceContext);
+export const useBalanceContext = () => {
+  const context = useContext(BalanceContext);
+  if (!context) {
+    throw new Error("useBalanceContext must be used within a BalanceProvider");
+  }
+  return context;
+};
 
 export const BalanceProvider = ({ children }: ProviderProps) => {
   const [earningsMock] = useState<arrayData>(generateEarningsData());
