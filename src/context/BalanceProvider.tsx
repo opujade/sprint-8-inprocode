4bc3af3d9618769d8144d61e84a12a0f45dde942
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-06-04 10:26:09 +0200

    expenses and earnings mock switched to state elements

--- src/context/BalanceProvider.tsx
+++ src/context/BalanceProvider.tsx
@@ -1,39 +1,41 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { calculateBalance } from "../utils/calculateBalance";
 import { BalanceContextInterface, ProviderProps } from "../types/types";
 import generateEarningsData from "../data/generateEarningsData";
 import generateExpensesData from "../data/generateExpensesData";
 import { calculatePercentageFirstTwoElements } from "../utils/calculatePercentageFirstTwoElements";
 import { formatChartData } from "../utils/formatChartData";
+import { arrayData } from "../types/types";
 
 const BalanceContext = createContext<BalanceContextInterface | undefined>(
-	undefined
+  undefined
 );
 
 export const useBalanceContext = () => useContext(BalanceContext);
 
-const expensesMock = generateExpensesData();
-const earningsMock = generateEarningsData();
-
 export const BalanceProvider = ({ children }: ProviderProps) => {
-	const balance = calculateBalance(earningsMock, expensesMock);
-	const todaysExpenses = expensesMock[0].amount;
-	const percentageYesterdayToday =
-		calculatePercentageFirstTwoElements(expensesMock);
-	const amountExpenses = expensesMock.map((expense) => expense.amount);
-	const dates = expensesMock.map((expense) => expense.date);
-	const expensesChartData = formatChartData(amountExpenses, dates, "Expenses");
+  const [earningsMock] = useState<arrayData>(generateEarningsData());
+  const [expensesMock] = useState<arrayData>(generateExpensesData());
+  console.log(expensesMock);
+  console.log(earningsMock);
+  const balance = calculateBalance(earningsMock, expensesMock);
+  const todaysExpenses = expensesMock[0].amount;
+  const percentageYesterdayToday =
+    calculatePercentageFirstTwoElements(expensesMock);
+  const amountExpenses = expensesMock.map((expense) => expense.amount);
+  const dates = expensesMock.map((expense) => expense.date);
+  const expensesChartData = formatChartData(amountExpenses, dates, "Expenses");
 
-	return (
-		<BalanceContext.Provider
-			value={{
-				balance,
-				todaysExpenses,
-				percentageYesterdayToday,
-				expensesChartData,
-			}}
-		>
-			{children}
-		</BalanceContext.Provider>
-	);
+  return (
+    <BalanceContext.Provider
+      value={{
+        balance,
+        todaysExpenses,
+        percentageYesterdayToday,
+        expensesChartData,
+      }}
+    >
+      {children}
+    </BalanceContext.Provider>
+  );
 };
