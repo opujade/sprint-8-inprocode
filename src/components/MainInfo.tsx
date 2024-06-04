575ea933c36df7b9f4287cc072867ae0e6eaa1f9
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-06-04 22:50:32 +0200

    main info text layout

--- src/components/MainInfo.tsx
+++ src/components/MainInfo.tsx
@@ -3,12 +3,23 @@ import { useBalanceContext } from "../context/BalanceProvider";
 export const MainInfo = () => {
   const { todaysExpenses, percentageYesterdayToday } = useBalanceContext();
   return (
-    <div>
-      <p>Today's Expenses: {todaysExpenses}€</p>
-      <p>
-        {percentageYesterdayToday > 0 && "+"}
-        {percentageYesterdayToday}% compared to yesterday
-      </p>
+    <div className="bg-white rounded-xl p-5 w-full text-black">
+      <div className="flex justify-between">
+        <div>
+          <h2 className="text-xl font-bold">Expenses - Last Week</h2>
+          <p className="text-gray-300 font-semibold text-xs">
+            Today's Expenses:
+          </p>
+          <p className="text-3xl font-bold mb-5">{todaysExpenses}€</p>
+        </div>
+        <div className="text-end flex flex-col justify-center font-semibold text-xs">
+          <p>
+            {percentageYesterdayToday > 0 && "+"}
+            {percentageYesterdayToday}%
+          </p>
+          <p>compared to yesterday</p>
+        </div>
+      </div>
     </div>
   );
 };
